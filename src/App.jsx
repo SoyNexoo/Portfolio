cdd39bc87ce17d318b381e6ddf053403a89fe5f6
SoyNexoo <Soynexoo@gmail.com>
2023-11-01 21:10:43 -0300

    About me style

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,9 @@ import Home from "./views/home/Home";
 import Nav from "./components/nav/Nav";
 import AboutMe from "./views/aboutme/AboutMe";
 import Projects from "./views/projects/Projects";
-import Stars from "./components/stars/Stars";
-
 function App() {
   return (
     <>
-      <Stars></Stars>
       <Nav />
       <Home></Home>
       <Projects></Projects>
--- src/views/aboutme/AboutMe.jsx
+++ src/views/aboutme/AboutMe.jsx
@@ -1,23 +1,86 @@
-import React from "react";
 import { motion } from "framer-motion";
 import s from "./AboutMe.module.css";
-import { ProfilePic } from "../../utils/Icons";
+import {
+  AkarIconsCssFill,
+  AkarIconsNodeFill,
+  AkarIconsReduxFill,
+  Express,
+  Figma,
+  MdiBootstrap,
+  MdiGithub,
+  MdiSlack,
+  ProfilePic,
+  React,
+  TablerBrandFramerMotion,
+  TeenyiconsJavascriptOutline,
+  WhhPgsql,
+} from "../../utils/Icons";
 import Reveal from "../../components/reveal/Reveal";
 const AboutMe = () => {
   return (
     <div className={s.container} id="aboutMe">
       <motion.div
-        initial={{ opacity: 0, y: 200 }}
+        initial={{ opacity: 0, y: 75 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.5 }}
         className={s.img}
       >
         <ProfilePic width="400" />
       </motion.div>
-      <div>
-        <Reveal>
-          <h1>JULIAN LUQUE</h1>
-        </Reveal>
+      <div className={s.info}>
+        <div className={s.name}>
+          <Reveal>
+            <h1>JULIAN LUQUE</h1>
+            <h5>
+              Joven <span>desarrollador web</span>
+            </h5>
+          </Reveal>
+        </div>
+        <div className={s.description}>
+          <p>
+            Graduado de HENRY en el stack PERN y con experiencia en diseño para
+            equipos E-Sports. Estoy en busca de mi primer trabajo como
+            desarrollador web. Apasionado por <span>React</span> y{" "}
+            <span>JavaScript</span>, me esfuerzo por crear experiencias web
+            sorprendentes.
+          </p>
+        </div>
+        <div className={s.stack}>
+          <h3>Mi stack:</h3>
+          <div className={s.rule}></div>
+          <div className={s.secContainer}>
+            <div className={s.section}>
+              <h3>Frontend</h3>
+              <div className={s.habs}>
+                <React width={40}></React>
+                <Figma width={40}></Figma>
+                <MdiBootstrap width={40}></MdiBootstrap>
+                <AkarIconsReduxFill width={40}></AkarIconsReduxFill>
+                <TablerBrandFramerMotion width={40}></TablerBrandFramerMotion>
+
+                <AkarIconsCssFill width={40}></AkarIconsCssFill>
+              </div>
+            </div>
+            <div className={s.section}>
+              <h3>Backend</h3>
+              <div className={s.habs}>
+                <TeenyiconsJavascriptOutline
+                  width={40}
+                ></TeenyiconsJavascriptOutline>
+                <Express width={40}></Express>
+                <AkarIconsNodeFill width={40}></AkarIconsNodeFill>
+              </div>
+            </div>
+          </div>
+          <div className={s.section}>
+            <h3>Herramientas</h3>
+            <div className={s.habs}>
+              <MdiGithub width={40}></MdiGithub>
+              <MdiSlack width={40}></MdiSlack>
+              <WhhPgsql width={40}></WhhPgsql>
+            </div>
+          </div>
+        </div>
       </div>
       <div className={s.blur}></div>
       <div className={s.blur2}></div>
